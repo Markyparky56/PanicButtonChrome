cb1f4acb75e23a52e3311d0f4a5aa09f6fb70eb5
Mark Andrew Ropper <mark@kiltandblade.com>
2018-06-10 13:50:19 +0100

    Fix options page, and make it a little wider Remove use of the word "simply" from the options page Reformat options.js to use 2-space tabs instead of 4 Increase popup panic button blacklist regex wildcard capturing limit to allow for longer urls

--- options.js
+++ options.js
@@ -1,121 +1,121 @@
 //Save options
 function save_options()
 {
-    let religious = document.getElementById("religious").checked;
-    let nsfwguard = document.getElementById("nsfwguard").checked;
-    let nsfwguard_redirectOnNSFWComments = document.getElementById("nsfwguard_comments").checked;
-    let safehaven = document.getElementById("nsfwguard_safehaven").value;
-    let popup = document.getElementById("popup").checked;
-    let popupSide = document.querySelector("input[name='popupSide']:checked").value;
+  let religious = document.getElementById("religious").checked;
+  let nsfwguard = document.getElementById("nsfwguard").checked;
+  let nsfwguard_redirectOnNSFWComments = document.getElementById("nsfwguard_comments").checked;
+  let safehaven = document.getElementById("nsfwguard_safehaven").value;
+  let popup = document.getElementById("popup").checked;
+  let popupSide = document.querySelector("input[name='popupSide']:checked").value;
 
-    // Check the safehaven url to ensure it is contains the http scheme, otherwise redirects will not work
-    let regex = /^https?:\/\//;
-    let regexResult = regex.exec(safehaven);
-    if(regexResult == null)
+  // Check the safehaven url to ensure it is contains the http scheme, otherwise redirects will not work
+  let regex = /^https?:\/\//;
+  let regexResult = regex.exec(safehaven);
+  if(regexResult == null)
+  {
+    safehaven = "http://" + safehaven; // http is often promoted to https, but https won't work if the site doesn't support it
+  }
+
+  // Get the popup blacklist in text form
+  let popupBlacklist = document.getElementById("popupBlacklist").value;
+  // Split into a list on semi-colons
+  popupBlacklist = popupBlacklist.split(';');
+  // Clean out empty values
+  for(let i = 0; i < popupBlacklist.length; i++)
+  {
+    if(popupBlacklist[i] === "")
     {
-      safehaven = "http://" + safehaven; // http is often promoted to https, but https won't work if the site doesn't support it
+      popupBlacklist.splice(i, 1);
+      i--;
     }
+  }
 
-    // Get the popup blacklist in text form
-    let popupBlacklist = document.getElementById("popupBlacklist").value;
-    // Split into a list on semi-colons
-    popupBlacklist = popupBlacklist.split(';');
-    // Clean out empty values
-    for(let i = 0; i < popupBlacklist.length; i++)
-    {
-      if(popupBlacklist[i] === "")
-      {
-        popupBlacklist.splice(i, 1);
-        i--;
-      }
-    }
-
-    chrome.storage.sync.set({
-        religiousEnabled: religious,
-        nsfwguardEnabled: nsfwguard,
-        nsfwguard_redirectOnNSFWComments: nsfwguard_redirectOnNSFWComments,
-        safehavenUrl: safehaven,
-        popupEnabled: popup,
-        popupBlacklist: popupBlacklist,
-        popupSide: popupSide
-    }, function() {
-        // Update status
-        let status = document.getElementById("status");
-        status.textContent = "Options saved.";
-        setTimeout(function() {
-            status.textContent = "";
-        }, 750);
-    });
+  chrome.storage.sync.set({
+    religiousEnabled: religious,
+    nsfwguardEnabled: nsfwguard,
+    nsfwguard_redirectOnNSFWComments: nsfwguard_redirectOnNSFWComments,
+    safehavenUrl: safehaven,
+    popupEnabled: popup,
+    popupBlacklist: popupBlacklist,
+    popupSide: popupSide
+  }, function() {
+    // Update status
+    let status = document.getElementById("status");
+    status.textContent = "Options saved.";
+    setTimeout(function() {
+      status.textContent = "";
+    }, 750);
+  });
 }
 
 // Restores setting state using preferences
 function restore_options()
 {
-    // Default religious off, nsfwguard to on and safehaven to /r/nofap
-    // Default popup box on, dummy example address for blacklist
-    chrome.storage.sync.get({
-        religiousEnabled: false,
-        nsfwguardEnabled: true,
-        nsfwguard_redirectOnNSFWComments: true,
-        safehavenUrl: "https://www.reddit.com/r/nofap/",
-        popupEnabled: true,
-        popupBlacklist: ["www.example.com"],
-        popupSide: "left"
-    }, function(items) {
-        document.getElementById("religious").checked = items.religiousEnabled;
-        document.getElementById("nsfwguard").checked = items.nsfwguardEnabled;
-        document.getElementById("nsfwguard_comments").checked = items.nsfwguard_redirectOnNSFWComments;        
-        document.getElementById("nsfwguard_safehaven").value = items.safehavenUrl;
-        document.getElementById("popup").checked = items.popupEnabled;
-        if(items.popupSide == "left")
-        {
-          document.getElementById("popupSideLeft").checked = true;
-        }
-        else
-        {
-          document.getElementById("popupSideRight").checked = true;
-        }
+  // Default religious off, nsfwguard to on and safehaven to /r/nofap
+  // Default popup box on, dummy example address for blacklist
+  chrome.storage.sync.get({
+    religiousEnabled: false,
+    nsfwguardEnabled: true,
+    nsfwguard_redirectOnNSFWComments: true,
+    safehavenUrl: "https://www.reddit.com/r/nofap/",
+    popupEnabled: true,
+    popupBlacklist: ["www.example.com"],
+    popupSide: "left"
+  }, function(items) {
+    document.getElementById("religious").checked = items.religiousEnabled;
+    document.getElementById("nsfwguard").checked = items.nsfwguardEnabled;
+    document.getElementById("nsfwguard_comments").checked = items.nsfwguard_redirectOnNSFWComments;        
+    document.getElementById("nsfwguard_safehaven").value = items.safehavenUrl;
+    document.getElementById("popup").checked = items.popupEnabled;
+    if(items.popupSide == "left")
+    {
+      document.getElementById("popupSideLeft").checked = true;
+    }
+    else
+    {
+      document.getElementById("popupSideRight").checked = true;
+    }
 
-        // Reconstruct black list from array
-        let blacklist = items.popupBlacklist.join(';');
-        if(blacklist[blacklist.length-1] !== ';') blacklist += ';'; // Append a final semi-colon if there isn't one
-        document.getElementById("popupBlacklist").textContent = blacklist;
+    // Reconstruct black list from array
+    let blacklist = items.popupBlacklist.join(';');
+    if(blacklist[blacklist.length-1] !== ';') blacklist += ';'; // Append a final semi-colon if there isn't one
+    document.getElementById("popupBlacklist").textContent = blacklist;
 
-        // Disable the nsfwguard safehaven for editing if nsfwgaurd is disabled
-        if(!items.nsfwguardEnabled) { document.getElementById("nsfwguard-safehaven").disabled=true; }
-    });
+    // Disable the nsfwguard safehaven for editing if nsfwgaurd is disabled
+    if(!items.nsfwguardEnabled) { document.getElementById("nsfwguard-safehaven").disabled=true; }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", restore_options);
 document.getElementById("save").addEventListener("click", save_options);
 document.addEventListener("DOMContentLoaded", function(event) {
-    let nsfwGuardCheckbox = document.querySelector("input[name=nsfwguard]");
-    let safehavenInput = document.querySelector("input[name=nsfwguard_safehaven]");
-    let nsfwguardComments = document.querySelector("input[name=nsfwguard_comments]");
-    nsfwGuardCheckbox.addEventListener("change", function(event) {
-        if(nsfwGuardCheckbox.checked) 
-        { 
-          safehavenInput.disabled=false; 
-          nsfwguardComments.disabled=false;          
-        }
-        else 
-        { 
-          safehavenInput.disabled=true; 
-          nsfwguardComments.disabled=true;
-        }
-    });
-    let popupCheckbox = document.querySelector("input[name=popup]");
-    let popupRadio = document.querySelectorAll("input[name=popupSide]");
-    popupCheckbox.addEventListener("change", function(event) {
-      if(popupCheckbox.checked)
-      {
-        popupRadio[0].disabled=false;
-        popupRadio[1].disabled=false;
-      }
-      else
-      {
-        popupRadio[0].disabled=true;
-        popupRadio[1].disabled=true;        
-      }
-    })
+  let nsfwGuardCheckbox = document.querySelector("input[name=nsfwguard]");
+  let safehavenInput = document.querySelector("input[name=nsfwguard_safehaven]");
+  let nsfwguardComments = document.querySelector("input[name=nsfwguard_comments]");
+  nsfwGuardCheckbox.addEventListener("change", function(event) {
+    if(nsfwGuardCheckbox.checked) 
+    { 
+      safehavenInput.disabled=false; 
+      nsfwguardComments.disabled=false;          
+    }
+    else 
+    { 
+      safehavenInput.disabled=true; 
+      nsfwguardComments.disabled=true;
+    }
+  });
+  let popupCheckbox = document.querySelector("input[name=popup]");
+  let popupRadio = document.querySelectorAll("input[name=popupSide]");
+  popupCheckbox.addEventListener("change", function(event) {
+    if(popupCheckbox.checked)
+    {
+      popupRadio[0].disabled=false;
+      popupRadio[1].disabled=false;
+    }
+    else
+    {
+      popupRadio[0].disabled=true;
+      popupRadio[1].disabled=true;        
+    }
+  })
 });
